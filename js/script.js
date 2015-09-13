ba140c23dfcc7777e5bfccc16d4149f4dff96fdf
ahmedakef <aemed.akef.1@gmail.com>
2015-09-13 11:57:21 +0200

    first try

---
+++ js/script.js
@@ -0,0 +1,58 @@
+ $(document).ready(function() {
+	/*	$('.intro-pic').each(function(){
+				$(this).addClass("fadeIn");
+		});
+	});*/
+
+ 	$(window).scroll(function() {
+		$('.basics').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+
+		$('.oop').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+
+		$('.django').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideLeft");
+			}
+		});
+
+		$('.Qt').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideDown");
+			}
+		});
+
+		$('.chal').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideExpandUp");
+			}
+		});
+
+
+
+	});
+
+
+ });
