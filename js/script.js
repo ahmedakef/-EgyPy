1bebf82f8107f17d32706a7f65ed7d59d531b85e
ahmedakef <aemed.akef.1@gmail.com>
2015-09-16 12:55:58 +0200

    dynamical navigation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@
 		});
 	});*/
 
+ 
+ 	/* change active class */
+
+   $(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+
+	});
+
+
+   /* make links go smoothly to its content */
+
+
+ 	$("#website-logo").click(function (){
+
+	    $('html, body').animate({
+	        scrollTop: $(".intro-background").offset().top
+	    }, 1000);
+
+ 	});
+
  	$(".courses-btn").click(function (){
 
 	    $('html, body').animate({
@@ -39,6 +60,7 @@
 
 
  	$(window).scroll(function() {
+
 		$('.basics').each(function(){
 		var imagePos = $(this).offset().top;
 
@@ -120,6 +142,97 @@
 			}
 		});
 
+/*      artical          */  
+
+		$('.artic-1').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+
+		$('.artic-2').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+
+		$('.artic-3').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideLeft");
+			}
+		});
+
+		$('.pic-1').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideDown");
+			}
+		});
+
+		$('.pic-2').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+
+		$('.pic-3').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideLeft");
+			}
+		});
+
+		$('.mahmoud-pic').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("bigEntrance");
+			}
+		});
+
+		$('.about-me h2').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+
+		$('.about-me p').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+450) {
+				$(this).addClass("slideLeft");
+			}
+		});
+
+		$('.brought-by').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+700) {
+				$(this).delay(5000).addClass("fadeIn");
+			}
+		});
 
 
 	});
