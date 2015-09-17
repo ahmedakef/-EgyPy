391458c835ccd5c8b7588af1ce6985e5c1fc7fb2
ahmedakef <aemed.akef.1@gmail.com>
2015-09-17 02:23:41 +0200

    put all links

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
  $(document).ready(function() {
-	/*	$('.intro-pic').each(function(){
-				$(this).addClass("fadeIn");
-		});
-	});*/
 
- 
+
+
+
+ 	/*load models 
+
+ 	  $('#article').click(function (){
+       $("#load-models").load("models/third-3-courses.html");              
+  	});
+	*/
+
+
+    $(".open-it").click(function(){
+        $(".open-source").toggle();
+        $("html, body").animate({ scrollTop: $(document).height()-$(window).height() });
+    });
+
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+
  	/* change active class */
+
+
 
    $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
@@ -14,7 +31,13 @@
 	});
 
 
+
+
+
    /* make links go smoothly to its content */
+
+
+
 
 
  	$("#website-logo").click(function (){
@@ -56,6 +79,11 @@
 	    }, 1000);
 
  	});
+
+
+
+
+ 	/* show courses   */
 
 
 
@@ -142,7 +170,14 @@
 			}
 		});
 
+
+
+
 /*      artical          */  
+
+
+
+
 
 		$('.artic-1').each(function(){
 		var imagePos = $(this).offset().top;
@@ -171,6 +206,11 @@
 			}
 		});
 
+
+		/* pics */ 
+
+
+
 		$('.pic-1').each(function(){
 		var imagePos = $(this).offset().top;
 
@@ -197,6 +237,13 @@
 				$(this).addClass("slideLeft");
 			}
 		});
+
+
+
+
+		/* about me */
+
+
 
 		$('.mahmoud-pic').each(function(){
 		var imagePos = $(this).offset().top;
